733d91948f24dded564714758ab165e6d765db26
velocityzen <velocityzen@gmail.com>
2016-10-14 23:57:40 -0400

    + editor config

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-"use strict";
-let less = require("less");
+'use strict';
+let less = require('less');
 
 module.exports = function(f, mat, options, next) {
-	mat.getContent(function(content) {
-		less.render(content.toString(), {
-			paths: [mat.src.dirname],
-			compress: !options.dev
-		}, function(err, output) {
-			let path = mat.dst().path;
-			if(err) {
-				next({
-					message: err.message,
-					file: err.filename === "input" ? path : err.filename,
-					line: err.line,
-					column: err.column,
-					extract: err.extract.join("\n")
-				});
-			} else {
-				f.addDependencies(output.imports, mat.id);
-				next(null, mat.setContent(output.css));
-			}
-		});
-	});
+  mat.getContent(function(content) {
+    less.render(content.toString(), {
+      paths: [ mat.src.dirname ],
+      compress: !options.dev
+    }, function(err, output) {
+      let path = mat.dst().path;
+      if (err) {
+        next({
+          message: err.message,
+          file: err.filename === 'input' ? path : err.filename,
+          line: err.line,
+          column: err.column,
+          extract: err.extract.join('\n')
+        });
+      } else {
+        f.addDependencies(output.imports, mat.id);
+        next(null, mat.setContent(output.css));
+      }
+    });
+  });
 };
